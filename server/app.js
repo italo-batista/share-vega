f8a0c44b58b61ab25e7a64ad4c963846d145b3fc
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2018-08-27 22:58:33 -0300

    Basic structure for app

---
+++ server/app.js
@@ -0,0 +1,33 @@
+/**
+ * Application server.
+ * Author:  Italo Batista, italohmb@gmail.com
+ * Last change: 08/2018
+ */
+
+/**
+ * Settings.
+ */
+
+var express = require("express");
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+var ENV = process.env.ENVIROMENT || "development";
+
+/**
+ * Api routes.
+ */
+
+/**
+ * Startup our app at http://localhost:3000
+ */
+
+app.listen(PORT);
+
+console.log("Magic happens on port " + PORT);
+
+/**
+ * Module exports.
+ */
+
+exports = module.exports = app;
