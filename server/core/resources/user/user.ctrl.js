a69ec6ea5102a79e04784c9d9b9a6b7a6b2ec82e
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2018-10-06 11:07:52 -0300

    Update user controller

--- server/core/resources/user/user.ctrl.js
+++ server/core/resources/user/user.ctrl.js
@@ -1,26 +1,18 @@
-/**
- * Controller for user.
- * Author:  Italo Batista, italohmb@gmail.com
- * Last change: 09/2018
- */
-
-const User = require("./user.model");
+const UserStoreClient = require("./user.store.client");
 const HttpStatus = require("../../../constants/httpStatus");
 
 exports.show = function(req, res) {
-  User.findById(req.params.user_id)
-  .catch(err => {
-    res.status(HttpStatus.BAD_REQUEST).send(err);
-  })
-  .then(user => {
-    res.status(HttpStatus.OK).json(user);
-  });
+  UserStoreClient.findUserById(req.params.user_id)
+    .catch(error => {
+      res.status(HttpStatus.BAD_REQUEST).json(error);
+    })
+    .then(fic => {
+      res.status(HttpStatus.OK).json(fic);
+    });
 };
 
 exports.create = function(req, res) {
-  let user = new User(req.body);
-  user
-    .save()
+  UserStoreClient.createUser(req.body)
     .catch(err => {
       res.status(HttpStatus.BAD_REQUEST).send(err);
     })
@@ -29,29 +21,35 @@ exports.create = function(req, res) {
     });
 };
 
-exports.update = function(req, res) {  
+exports.update = function(req, res) {
   User.findById(req.params.user_id)
-  .catch(err => {
-    res.status(HttpStatus.BAD_REQUEST).send(err);
-  })
-  .then(user => {
-    if (req.body.name) user.name = req.body.name;
-    if (req.body.username) user.username = req.body.username;
-    if (req.body.email) user.email = req.body.email;
-    if (req.body.gender) user.gender = req.body.gender;
-    if (req.body.description) user.description = req.body.description;
-    
-    user
-      .save()
-      .catch(err => {
-        res.status(HttpStatus.BAD_REQUEST).send(err);
-      })
-      .then(updatedUser => {
-        res.status(HttpStatus.OK).json(updatedUser);
-      });
-  });
+    .catch(err => {
+      res.status(HttpStatus.BAD_REQUEST).send(err);
+    })
+    .then(user => {
+      if (req.body.name) user.name = req.body.name;
+      if (req.body.username) user.username = req.body.username;
+      if (req.body.email) user.email = req.body.email;
+      if (req.body.gender) user.gender = req.body.gender;
+      if (req.body.description) user.description = req.body.description;
+
+      user
+        .save()
+        .catch(err => {
+          res.status(HttpStatus.BAD_REQUEST).send(err);
+        })
+        .then(updatedUser => {
+          res.status(HttpStatus.OK).json(updatedUser);
+        });
+    });
 };
 
 exports.delete = function(req, res) {
-  res.json("NOT IMPLEMENTED: Visualization delete DELETE");
+  UserStoreClient.deleteUser(req.params.visualization_id)
+    .catch(error => {
+      res.status(HttpStatus.BAD_REQUEST).json(error);
+    })
+    .then(deletedUser => {
+      res.status(HttpStatus.OK).json(deletedUser);
+    });
 };
