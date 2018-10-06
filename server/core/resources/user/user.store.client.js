82baed4f917be8d4578d35d319123f2ca4dec58b
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2018-10-06 10:54:31 -0300

    Create db client to User operations

---
+++ server/core/resources/user/user.store.client.js
@@ -0,0 +1,17 @@
+const User = require("./user.model");
+
+exports.findUserById = function(id) {
+  return User.find({ _id: id }, { _id: false }).exec();
+};
+
+exports.createUser = function(params) {
+  return new User(params).save();
+};
+
+exports.updateUser = function(id, params) {
+  return User.updateOne({ _id: id }, { $set: params }).exec();
+};
+
+exports.deleteUser = function(id) {
+  return User.deleteOne({ _id: id }).exec();
+};
