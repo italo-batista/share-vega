f4206e3d6ed7d3f02c7f39d3881f8eca46cc4170
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2018-11-26 09:42:59 -0300

    Add route to unstar vis

--- server/core/resources/star/star.ctrl.js
+++ server/core/resources/star/star.ctrl.js
@@ -1,5 +1,6 @@
 const VisStoreClient = require("./../visualization/vis.store.client");
 const HttpStatus = require("../../../constants/httpStatus");
+const utils = require("../.././../utils/util");
 
 exports.create = async function(req, res) {
   let vis = await VisStoreClient.findOneVisById(req.body.visualization_id);
@@ -7,7 +8,6 @@ exports.create = async function(req, res) {
   let addUserStar = [req.body.user_id];
   let newVisStars = oldVisStars.concat(addUserStar);
   let paramsToUpdate = {"userStars": newVisStars};
-  console.log(paramsToUpdate);
   VisStoreClient.updateVis(req.body.visualization_id, paramsToUpdate)
   .catch(err => {
     res.status(HttpStatus.BAD_REQUEST).send(err);
@@ -17,12 +17,16 @@ exports.create = async function(req, res) {
   });
 };
 
-exports.delete = function(req, res) {
-  VisStoreClient.deleteVis(req.params.visualization_id)
-    .catch(error => {
-      res.status(HttpStatus.BAD_REQUEST).json(error);
-    })
-    .then(deleted => {
-      res.status(HttpStatus.OK).json(deleted);
-    });
+exports.delete = async function(req, res) {
+  let vis = await VisStoreClient.findOneVisById(req.body.visualization_id);
+  utils.removeItemFromArray(vis.userStars, req.body.user_id);
+
+  let paramsToUpdate = {"userStars": vis.userStars};
+  VisStoreClient.updateVis(req.body.visualization_id, paramsToUpdate)
+  .catch(err => {
+    res.status(HttpStatus.BAD_REQUEST).send(err);
+  })
+  .then(updatedVis => {
+    res.status(HttpStatus.OK).json(updatedVis);
+  });
 };
